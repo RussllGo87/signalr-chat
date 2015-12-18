0496fbea1d0560355e07d39e46f6f6711e3669db
gongguopei <gongguopei87@gmail.com>
2015-12-18 15:39:59 +0800

    欢迎界面自动版本更新检查

--- app/src/main/java/net/pingfang/signalr/chat/activity/MainActivity.java
+++ app/src/main/java/net/pingfang/signalr/chat/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package net.pingfang.signalr.chat.activity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,22 +17,37 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.model.LatLng;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import net.pingfang.signalr.chat.R;
 import net.pingfang.signalr.chat.constant.app.AppConstants;
 import net.pingfang.signalr.chat.location.LocationListenerImpl;
 import net.pingfang.signalr.chat.location.LocationNotify;
+import net.pingfang.signalr.chat.net.HttpBaseCallback;
+import net.pingfang.signalr.chat.net.OkHttpCommonUtil;
+import net.pingfang.signalr.chat.ui.dialog.DoubleButtonDialogFragment;
+import net.pingfang.signalr.chat.util.GlobalApplication;
 import net.pingfang.signalr.chat.util.ImageUtils;
 import net.pingfang.signalr.chat.util.SharedPreferencesHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements LocationNotify {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final String URL_APP_VERSION_CHECK = GlobalApplication.URL_WEB_API_HOST + "/api/WebAPI/User/GetApkVersion";
+    public static final String URL_APP_DOWNLOAD = GlobalApplication.URL_WEB_API_HOST + "/hale.apk";
 
     SharedPreferencesHelper sharedPreferencesHelper;
 
@@ -39,19 +57,19 @@ public class MainActivity extends AppCompatActivity implements LocationNotify {
     TextView btn_login;
     TextView btn_register;
     Handler mHandler = new Handler(Looper.getMainLooper());
-    private LatLng currentLatLng;
-    private LocationClient locationClient;
-    private LocationListenerImpl locationListener;
+
+    PackageInfo info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferencesHelper = SharedPreferencesHelper.newInstance(getApplicationContext());
         initLocation();
         initView();
+        checkLatestVersion();
 
-        sharedPreferencesHelper = SharedPreferencesHelper.newInstance(getApplicationContext());
         String uid = sharedPreferencesHelper.getStringValue(AppConstants.KEY_SYS_CURRENT_UID);
         if(!TextUtils.isEmpty(uid)) {
             mHandler.postDelayed(new Runnable() {
@@ -82,8 +100,8 @@ public class MainActivity extends AppCompatActivity implements LocationNotify {
         option.setIsNeedAddress(true);
         option.setIgnoreKillProcess(false);
 
-        locationClient = new LocationClient(getApplicationContext(),option);
-        locationListener = new LocationListenerImpl(this);
+        LocationClient locationClient = new LocationClient(getApplicationContext(),option);
+        LocationListenerImpl locationListener = new LocationListenerImpl(this);
         locationClient.registerLocationListener(locationListener);
         locationClient.start();
     }
@@ -91,10 +109,10 @@ public class MainActivity extends AppCompatActivity implements LocationNotify {
     @Override
     public void updateLoc(BDLocation bdLocation) {
 
-        Log.d(TAG,"update Location");
+        Log.d(TAG, "update Location");
 
         if (bdLocation != null) {
-            currentLatLng = new LatLng(bdLocation.getLatitude(),
+            LatLng currentLatLng = new LatLng(bdLocation.getLatitude(),
                     bdLocation.getLongitude());
 
             sharedPreferencesHelper.putStringValue(AppConstants.KEY_SYS_LOCATION_LAT, Double.toString(currentLatLng.latitude));
@@ -127,6 +145,83 @@ public class MainActivity extends AppCompatActivity implements LocationNotify {
             @Override
             public void onClick(View view) {
                 register(view);
+            }
+        });
+
+        try {
+            info = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), R.string.toast_current_app_version_info_check_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void checkLatestVersion() {
+        OkHttpCommonUtil okHttp = OkHttpCommonUtil.newInstance(getApplicationContext());
+        okHttp.getRequest(URL_APP_VERSION_CHECK, null, new HttpBaseCallback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                super.onFailure(request, e);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), R.string.network_error, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                String responseStr = response.body().string();
+                Log.d(TAG, "URL_APP_VERSION_CHECK return == " + responseStr);
+                JSONObject jsonObject = null;
+                try {
+                    jsonObject = new JSONObject(responseStr);
+                    int versionCode = jsonObject.getInt("VersionCode");
+                    //                    String versionName = jsonObject.getString("VersionName");
+
+                    int currentVersionCode = info.versionCode;
+                    //                    String currentVersionName = info.versionName;
+
+                    if (versionCode > currentVersionCode) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                DoubleButtonDialogFragment dialogFragment = DoubleButtonDialogFragment.newInstance(
+                                        getApplicationContext().getString(R.string.dialog_message_url_latest_app_download),
+                                        new DoubleButtonDialogFragment.DoubleButtonDialogClick() {
+                                            @Override
+                                            public void onPositiveButtonClick() {
+                                                Intent intent = new Intent();
+                                                intent.setAction(Intent.ACTION_VIEW);
+                                                intent.setData(Uri.parse(URL_APP_DOWNLOAD));
+                                                if (intent.resolveActivity(getPackageManager()) != null) {
+                                                    startActivity(intent);
+                                                }
+                                            }
+                                        });
+                                dialogFragment.show(getSupportFragmentManager(), "DoubleButtonDialogFragment");
+                            }
+                        });
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), R.string.toast_app_name_version_latest, Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), R.string.toast_latest_app_version_info_check_error, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
             }
         });
     }
